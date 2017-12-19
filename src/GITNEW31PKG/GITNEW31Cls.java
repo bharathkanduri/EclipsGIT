5aecbe2b484bebdda50f3ef04cf665ae7515a647
bharathkanduri <bhkumarkanduri@gmail.com>
2017-12-19 13:05:19 +0530

    new project added

---
+++ src/GITNEW31PKG/GITNEW31Cls.java
@@ -0,0 +1,11 @@
+package GITNEW31PKG;
+
+public class GITNEW31Cls {
+
+	public static void main(String[] args) {
+		System.out.println("H1");
+		System.out.println("H2");
+		System.out.println("H3");
+	}
+
+}
